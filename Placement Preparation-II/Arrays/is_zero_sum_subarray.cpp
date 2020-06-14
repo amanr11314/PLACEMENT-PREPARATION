6d304c19a1cf5dcd48464353c77bdff03277dcb6
Aman Raj <amanr11314@gmail.com>
2020-06-14 13:25:41 +0530

    Added zero sum subarray

---
+++ Placement Preparation-II/Arrays/is_zero_sum_subarray.cpp
@@ -0,0 +1,27 @@
+//to check if there is a subarray with sum=0
+bool isZeroSubarray(int arr[],int n){
+
+    /*
+    [3,4,-1,4,3,-6,-7,-2]
+         <------->
+            0
+    <-->
+      x
+    <------------>
+          y
+    In such case we see that: x==y
+    i.e. the current sum, y already exists in hashset....
+    so, we simply return true;
+    */
+    unordered_set<int> m;
+    int sum = 0;
+    m.insert(sum);
+    for(int i=0;i<n;++i){
+        cout<<"sum = "<<sum<<'\n';
+        sum+=arr[i];
+        if(m.find(sum)!=m.end())    return 1;
+        m.insert(sum);
+    }
+
+    return false;
+}
