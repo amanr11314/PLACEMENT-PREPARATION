dede413b44f279e71bbaba3c240e5d545649b015
Aman Raj <amanr11314@gmail.com>
2020-07-09 20:29:57 +0530

    subset array approach3 added

--- Placement Preparation-II/Arrays/is_array_subset.cpp
+++ Placement Preparation-II/Arrays/is_array_subset.cpp
@@ -28,3 +28,37 @@ bool approach2(int a[],int m,int b[],int n){
     
     return true;
 }
+bool approach3(int a[],int b[],int an,int bn){
+
+    //We use here merge like approach to check if element in b[]
+    //occurs in a[] or not.
+
+    //Sort both arrays..
+    sort(a,a+an);   //O(an*log an)
+    sort(b,b+bn);   //O(bn*log bn)
+
+    int i=0,j=0;
+
+    while(i<an && j<bn){
+        //We increase both pointer if elements are equal
+        if(a[i]==b[j]){
+            i++;
+            j++;
+        }
+        //if a[i]<b[j] then move pointer i till we reach the position 
+        //a[i]>=b[j]
+        else if(a[i]<b[j]){
+            i++;
+        }
+        //terminate if a[i]>b[j] since, this is case when we did not
+        //find the element in a[] since a is sorted, the element can't be
+        //anywhere ahead in the a[].
+        else{
+            return false;
+        }
+    }
+
+    //check if we visited and matched all elements of b[] in a[]
+    return j>=bn;
+
+}
