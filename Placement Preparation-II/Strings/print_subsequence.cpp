4864edacd2183c2645ed7fb4a4a53438e40df839
Aman Raj <amanr11314@gmail.com>
2020-07-15 22:23:17 +0530

    fix lexicographical and duplicate subsequences

--- Placement Preparation-II/Strings/print_subsequence.cpp
+++ Placement Preparation-II/Strings/print_subsequence.cpp
@@ -27,9 +27,17 @@
     	cd
     d
 */
-//for lexicographical order:- store subsequences into multiset
+//for lexicographical order:- store subsequences into set/multiset based upon if we want to
+//include dupliate subsequences or not.
+set<string> ss; //for lecxicograhically and unique subsequences
+// multiset<string> ss;  //for lecxicograhically and duplicate subsequences
 void printSubsequence(string s,int index=0,string t="") {
+    //clear ss before the first call.
+    if(index==0)    ss.clear();
+    //for printing
     cout<<t<<'\n';
+    //for inserting into set
+    ss.insert(t);
     
     for(int i=index;i<s.length();++i) {
         // including s[i] in subsequence
